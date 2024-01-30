4248bb096c15ebf7e8d7a4911ec64ac54937eb55
Tyler Myracle <tyler.myracle@gmail.com>
2024-01-29 22:26:02 -0600

    part 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create a default user
+user = User.where(email: "test@tylermyracle.com").first_or_initialize
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
