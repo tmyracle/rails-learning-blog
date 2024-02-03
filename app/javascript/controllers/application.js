a5065fc36ffebdf63cc8306cae956fa5b8b1614b
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-03 16:24:04 -0600

    part 20

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,19 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+document.addEventListener("DOMContentLoaded", function () {
+  const fileInput = document.getElementById("file_input");
+  const fileNameDisplay = document.getElementById("file_name");
+
+  fileInput.addEventListener("change", (event) => {
+    const fileName = event.target.files[0]?.name;
+    fileNameDisplay.textContent = fileName || "No file chosen";
+  });
+});
+
+export { application };
